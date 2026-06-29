cd65d448d324ad489462270408322416f97c460f
Pedro Medina Stracieri <pedromstracieri@gmail.com>
2026-06-29 00:16:53 -0300

    feat: added JWT authentication filter and security configuration

---
+++ dfd-service/src/main/java/com/tcc/dfd_service/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.tcc.dfd_service.config;
+
+import com.tcc.dfd_service.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrfConfig -> csrfConfig.disable())
+                .authorizeHttpRequests(
+                        request ->
+                                request
+                                        .requestMatchers("/**")
+                                        .permitAll());
+        http.formLogin(Customizer.withDefaults());
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
