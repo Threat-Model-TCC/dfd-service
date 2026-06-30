9ef3fa1c20ff77f515e91b6085d5b8a3db09a0b4
Pedro Medina Stracieri <pedromstracieri@gmail.com>
2026-06-30 10:46:14 -0300

    feat: update JwtService to use RSA public key for JWT verification

--- dfd-service/src/main/java/com/tcc/dfd_service/service/JwtService.java
+++ dfd-service/src/main/java/com/tcc/dfd_service/service/JwtService.java
@@ -4,23 +4,30 @@ import com.tcc.dfd_service.enums.JwtTokenType;
 import com.tcc.dfd_service.vo.JwtToken;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.SecretKey;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Date;
 import java.util.Objects;
 
 @Service
 public class JwtService {
 
-    private final String JWT_SECRET = "0Hm08ePzQrbOQHJdtm0oi0OglriYYKID8GX+LHAh7mE=";
+    private final String JWT_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAz5pruXhkpj7+V3PD6TMU\n" +
+            "19K8Lg1wkBxseuUsc3wU5Ad02jRojn6vCX9q1Yv0LeEhj1cQm/YH00/v/2tnbf9N\n" +
+            "HdxWZlzzVTiPsFRB6tbQJcne0A2OoiSy7oKB5xMQsf6TNbZrWJ3cHwqbTEz4/jk2\n" +
+            "IvXYy3Lw6KyoPDdBzdeMfplV51UI6xDju+3xONMPxk57tvGXmgdfzJiU0R294I3V\n" +
+            "w2J0EcuUr063eaBraZaW6DTZ8j8egk/cYKqZcJB7lOZxdk7mFWL1KhtCJIXn3vl7\n" +
+            "0HSFedPCxlVUbIdtDTL3QM5eUaOINCGOilyKRbIB5GlhcUwGGSYOmF3ESkRlxBEa\n" +
+            "dwIDAQAB";
 
     private final String TOKEN_TYPE_CLAIM = "token-type";
 
     public String extractUsername(JwtToken jwtToken) {
         return Jwts.parser()
-                .verifyWith(getSignInKey())
+                .verifyWith(getSignInPublicKey())
                 .build()
                 .parseSignedClaims(jwtToken.getValue())
                 .getPayload()
@@ -39,7 +46,7 @@ public class JwtService {
 
     private Date extractExpiration(JwtToken jwtToken) {
         return Jwts.parser()
-                .verifyWith(getSignInKey())
+                .verifyWith(getSignInPublicKey())
                 .build()
                 .parseSignedClaims(jwtToken.getValue())
                 .getPayload()
@@ -48,7 +55,7 @@ public class JwtService {
 
     private JwtTokenType extractTokenType(JwtToken token) {
         String type = Jwts.parser()
-                .verifyWith(getSignInKey())
+                .verifyWith(getSignInPublicKey())
                 .build()
                 .parseSignedClaims(token.getValue())
                 .getPayload()
@@ -56,8 +63,13 @@ public class JwtService {
         return JwtTokenType.valueOf(type);
     }
 
-    private SecretKey getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(JWT_SECRET);
-        return Keys.hmacShaKeyFor(keyBytes);
+    private PublicKey getSignInPublicKey() {
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            byte[] publicKeyBytes = Decoders.BASE64.decode(JWT_PUBLIC_KEY);
+            return keyFactory.generatePublic(new X509EncodedKeySpec(publicKeyBytes));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
